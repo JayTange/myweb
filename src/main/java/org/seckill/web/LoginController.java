606d3d2eacf9d8a380e2fb3756f8b1f993881155
tangj <tangj@koal.com>
2017-09-18 13:26:09 +0800

    update

--- src/main/java/org/seckill/web/LoginController.java
+++ src/main/java/org/seckill/web/LoginController.java
@@ -1,13 +1,23 @@
 package org.seckill.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.annotation.Annotation;
+
 @Controller
+@RequestMapping
 public class LoginController {
-    @RequestMapping("/login")
-    public ModelAndView login(){
-        return new ModelAndView("login");
+    @RequestMapping(value = "/first", method = RequestMethod.GET)
+    public ModelAndView login(Model model) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "你好");
+        mv.setViewName("first");
+        return mv;
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/common/top.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tangj
+  Date: 2017/9/18
+  Time: 13:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1 style="color:blue;">网页头部</h1>
+</body>
+</html>
